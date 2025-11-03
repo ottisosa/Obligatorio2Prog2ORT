0677f19d6c998b20f0cf96257effe4d9cd12fac0
Octavio Sosa <121409253+ottisosa@users.noreply.github.com>
2025-11-02 21:18:04 -0300

    Add files via upload

---
+++ obligatorio2/src/dominio/Obligatorio2.java
@@ -0,0 +1,13 @@
+package dominio;
+
+// Autores: Santiago Quintana (327886), Octavio Sosa (363131)
+
+public class Obligatorio2 {
+
+   
+    public static void main(String[] args) {
+
+        
+    }
+    
+}
