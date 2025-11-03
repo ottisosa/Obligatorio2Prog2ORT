204d0c4e45e94611666435f4f5755b76b0860ea0
Santiago Quintana <SQ327886@fi365.ort.edu.uy>
2025-11-03 20:23:52 -0300

    a

--- obligatorio2/src/dominio/Obligatorio2.java
+++ obligatorio2/src/dominio/Obligatorio2.java
@@ -1,11 +1,22 @@
 package dominio;
+import interfaz.*;
 
 // Autores: Santiago Quintana (327886), Octavio Sosa (363131)
 
+import interfaz.SplashScreen;
 public class Obligatorio2 {
 
    
     public static void main(String[] args) {
+        
+        SplashScreen splash = new SplashScreen();
+        splash.setVisible(true);
+        try{
+            Thread.sleep(4000);
+        }
+        catch(InterruptedException e){
+        }
+        splash.dispose();
         
         
     }
