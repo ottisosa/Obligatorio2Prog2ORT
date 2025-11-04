2df0487a94f1bd4d66745fa6f7d55a54c4c592fa
Santiago Quintana <SQ327886@fi365.ort.edu.uy>
2025-11-03 23:05:20 -0300

    a

--- obligatorio2/src/interfaz/SplashScreen.java
+++ obligatorio2/src/interfaz/SplashScreen.java
@@ -7,38 +7,29 @@ public class SplashScreen extends javax.swing.JFrame {
 
     public SplashScreen() {
         initComponents();
-        this.setLocationRelativeTo(null);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setBackground(new java.awt.Color(0, 0, 0));
-        setPreferredSize(new java.awt.Dimension(400, 300));
-        setResizable(false);
-        getContentPane().setLayout(null);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
 
-        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
-        jPanel1.setLayout(null);
-
-        jLabel1.setText("Autores: Santiago Quintana (327886) - Octavio Sosa (363131)");
-        jPanel1.add(jLabel1);
-        jLabel1.setBounds(10, 240, 350, 16);
-
-        getContentPane().add(jPanel1);
-        jPanel1.setBounds(0, 0, 390, 290);
-
-        setBounds(0, 0, 400, 300);
+        pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
