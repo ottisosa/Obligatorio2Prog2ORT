80f416ab88cbd3b2a1f0edfd1475085d5ad17794
Santiago Quintana <SQ327886@fi365.ort.edu.uy>
2025-11-12 20:42:50 -0300

    a

--- obligatorio2/src/dominio/Obligatorio2.java
+++ obligatorio2/src/dominio/Obligatorio2.java
@@ -9,14 +9,7 @@ public class Obligatorio2 {
    
     public static void main(String[] args) {
         
-        String n = "HOLAAAAA";
         Sistema sistema = new Sistema();
-        if(sistema.equals(n)){
-            System.out.println("funciona");
-        }
-        else{
-            System.out.println("Funciona");
-        }
         SplashScreen splash = new SplashScreen(sistema);
     
     }
