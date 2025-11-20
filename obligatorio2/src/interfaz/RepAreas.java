faee9d239c0eb32ec1a0e8d376d9074047d01218
ottisosa <ottisosa10@gmail.com>
2025-11-20 00:45:00 -0300

    FUNCIONAAA

--- obligatorio2/src/interfaz/RepAreas.java
+++ obligatorio2/src/interfaz/RepAreas.java
@@ -2,9 +2,14 @@ package interfaz;
 
 // Autores: Santiago Quintana (327886), Octavio Sosa (363131)
 import dominio.*;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class RepAreas extends javax.swing.JFrame implements Observer {
 
@@ -15,42 +20,59 @@ public class RepAreas extends javax.swing.JFrame implements Observer {
         this.modelo.addObserver(this);
         this.cargarLista();
 
-
     }
 
     public void cargarLista() {
-        
-       listaAreas.setCellRenderer(new Renderer());
 
-        
-       this.modelo.ordenarAreaPorcentaje(this.modelo.getListaAreas());
-       this.listaAreas.setListData(this.modelo.getListaAreas().toArray());
+        listaAreas.setCellRenderer(new Renderer());
 
-       
+        this.modelo.ordenarAreaPorcentaje(this.modelo.getListaAreas());
+        this.listaAreas.setListData(this.modelo.getListaAreas().toArray());
+
     }
 
-    public void cargarListaEmp(Area area) {
-        
+    public void cargarPanelEmp(Area area) {
+
+        panelEmpleados.removeAll();
+        panelEmpleados.setLayout(new GridLayout(0, 3));
 
         ArrayList<Empleado> empleados = this.modelo.getListaEmpleados();
-
         ArrayList<Empleado> empleadosFiltrados = new ArrayList<>();
 
-        for (Empleado empleado : empleados) {
-            if (empleado.getArea().equals(area)) {
-                empleadosFiltrados.add(empleado);
-            }
-
-            if (empleadosFiltrados.isEmpty()) {
-
-                String[] mensaje = {"Sin empleados"};
-
-                this.listaEmp.setListData(mensaje);
-            } else {
-
-                this.listaEmp.setListData(empleadosFiltrados.toArray());
+        for (Empleado emp : empleados) {
+            if (emp.getArea().equals(area)) {
+                empleadosFiltrados.add(emp);
             }
         }
+
+        if (empleadosFiltrados.isEmpty()) {
+            panelEmpleados.add(new JLabel("Sin empleados"));
+        } else {
+
+            int salarioMax = 0;
+            for (Empleado emp : empleadosFiltrados) {
+                salarioMax = Math.max(salarioMax, emp.getSalario());
+            }
+
+            for (Empleado emp : empleadosFiltrados) {
+
+                JButton b = new JButton(emp.getNombre());
+
+                int salario = emp.getSalario();
+                int colorValue = (salarioMax == 0) ? 30 : 30 + (salario * 225) / salarioMax;
+                b.setBackground(new Color(0, 0, colorValue));
+                b.setForeground(Color.WHITE);
+
+                b.addActionListener(e -> {
+                    JOptionPane.showMessageDialog(this, " Nombre: " + emp.getNombre() + "\n Salario: " + emp.getSalario() + "\n CI: " + emp.getCi() + "\n Celular: " + emp.getCelular() + "\n Area: " + emp.getArea(), "Empleado", JOptionPane.INFORMATION_MESSAGE);
+                });
+
+                panelEmpleados.add(b);
+            }
+        }
+
+        panelEmpleados.revalidate();
+        panelEmpleados.repaint();
 
     }
 
@@ -59,7 +81,7 @@ public class RepAreas extends javax.swing.JFrame implements Observer {
         this.cargarLista();
 
         if (this.listaAreas.getSelectedValue() != null) {
-            this.cargarListaEmp((Area) this.listaAreas.getSelectedValue());
+            this.cargarPanelEmp((Area) this.listaAreas.getSelectedValue());
         }
     }
 
@@ -71,9 +93,8 @@ public class RepAreas extends javax.swing.JFrame implements Observer {
         jScrollPane1 = new javax.swing.JScrollPane();
         listaAreas = new javax.swing.JList();
         jLabel1 = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        listaEmp = new javax.swing.JList();
         jLabel4 = new javax.swing.JLabel();
+        panelEmpleados = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Reporte estado de areas");
@@ -103,20 +124,20 @@ public class RepAreas extends javax.swing.JFrame implements Observer {
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Áreas:");
 
-        listaEmp.setBackground(new java.awt.Color(255, 255, 255));
-        listaEmp.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 4, 4, 0, new java.awt.Color(0, 0, 0)));
-        listaEmp.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
-        listaEmp.setForeground(new java.awt.Color(0, 0, 0));
-        listaEmp.setModel(new javax.swing.AbstractListModel() {
-            String[] strings = { "Sin area seleccionada" };
-            public int getSize() { return strings.length; }
-            public Object getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane2.setViewportView(listaEmp);
-
         jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Empleados:");
+
+        javax.swing.GroupLayout panelEmpleadosLayout = new javax.swing.GroupLayout(panelEmpleados);
+        panelEmpleados.setLayout(panelEmpleadosLayout);
+        panelEmpleadosLayout.setHorizontalGroup(
+            panelEmpleadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 410, Short.MAX_VALUE)
+        );
+        panelEmpleadosLayout.setVerticalGroup(
+            panelEmpleadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -130,23 +151,26 @@ public class RepAreas extends javax.swing.JFrame implements Observer {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(17, 17, 17)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(120, 120, 120)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 368, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(26, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(120, 120, 120)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(58, 58, 58)
+                        .addComponent(panelEmpleados, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(46, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(32, 32, 32)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE)
+                    .addComponent(panelEmpleados, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(159, Short.MAX_VALUE))
         );
 
@@ -158,7 +182,7 @@ public class RepAreas extends javax.swing.JFrame implements Observer {
 
     private void listaAreasValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_listaAreasValueChanged
 
-        this.cargarListaEmp((Area) this.listaAreas.getSelectedValue());
+        this.cargarPanelEmp((Area) this.listaAreas.getSelectedValue());
 
     }//GEN-LAST:event_listaAreasValueChanged
 
@@ -168,9 +192,8 @@ public class RepAreas extends javax.swing.JFrame implements Observer {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JList listaAreas;
-    private javax.swing.JList listaEmp;
+    private javax.swing.JPanel panelEmpleados;
     // End of variables declaration//GEN-END:variables
     private Sistema modelo;
 }
